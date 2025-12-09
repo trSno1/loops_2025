9185f35add4ba82f589f8ff97638b0e67fd88351
trSno1 <aguilarjayden93@gmail.com>
2025-12-09 21:08:46 +0000

    edits

--- 1_loops.py
+++ 1_loops.py
@@ -1,36 +1,36 @@
-# # Example Practice:
-# # Given this list of fruits:
-# fruits = ["apple", "banana", "cherry", "date"]
+# Example Practice:
+# Given this list of fruits:
+fruits = ["apple", "banana", "cherry", "date"]
 
-# # Challenge:
-# # Use a for loop to print each fruit on a new line.
-# print(fruits[0])
-# print(fruits[1])
-# print(fruits[2])
-# for fruit in fruits:
-#     print(fruit)
-# # i just worked with loops
+# Challenge:
+# Use a for loop to print each fruit on a new line.
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+for fruit in fruits:
+    print(fruit)
+# i just worked with loops
 
-# # Given a list of school subjects:    
-# subjects = ["Math", "Science", "History", "Art"]
-# for subject in subjects:
-#         if subject == "History":
-#             break
-#         print(subject)
+# Given a list of school subjects:    
+subjects = ["Math", "Science", "History", "Art"]
+for subject in subjects:
+        if subject == "History":
+            break
+        print(subject)
 
-# for index in range*(len(subjects)):
-#     print("Subject " + str(index) + ":" + subject[index])
+for index in range*(len(subjects)):
+    print("Subject " + str(index) + ":" + subject[index])
 
-# # Challenge:
-# # Use a for loop and range to print each subject along with its index:
-# # Example output: "Subject 0: Math"
-# numbers = [1,2,3,4]
-# total = 0
-# for number in numbers:
-#      total += number
-# print("total:" , total)
-# # # Given:
-# numbers = [5, 10, 15, 20]
+# Challenge:
+# Use a for loop and range to print each subject along with its index:
+# Example output: "Subject 0: Math"
+numbers = [1,2,3,4]
+total = 0
+for number in numbers:
+     total += number
+print("total:" , total)
+# # Given:
+numbers = [5, 10, 15, 20]
 
 # # Challenge:
 # # Use a for loop to add all the numbers and print the total.
@@ -41,3 +41,20 @@ total = 0
 for number in new_numbers:
      total += number
      print("Total:", total)
+
+
+
+
+
+
+
+
+list = (list(range): 5,25)
+total = 0
+for num in list:
+     total += num + 0
+
+
+
+
+print(sum)
