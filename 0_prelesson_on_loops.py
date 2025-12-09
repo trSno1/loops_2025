c46b60ba818efde844d9b1131181422bdba67855
trSno1 <aguilarjayden93@gmail.com>
2025-12-09 20:22:21 +0000

    edits

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,21 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+#if looping is available then that means it is an iteration
+for x in range(1,11):
+    print(x)
+
+print("HAPPY NEW YEAR")
+for x in range(1,21):
+    if x == 13:
+        break
+else:
+    print(x)
+
+print("HAPPY NEW YEAR")
+for x in range(1,21):
+    if x == 13:
+        continue
+else:
+    print(x)
+
+    
